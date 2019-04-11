b42811be8e115095c3f0f756e9ed6f96f2003218
Vinicius <believe.182@hotmail.com>
2019-04-11 16:03:55 +0000

    Starting Q&A-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import NavBar from "./Components/NavBar/NavBar";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <p>Work in progress.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
